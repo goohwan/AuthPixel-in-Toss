e20ecc052d6d5d6bae89535c4b65668ecbe05154
goohwan <goohwan@gmail.com>
2025-11-30 10:14:06 +0900

    11

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ with st.sidebar:
             <img src="https://img.buymeacoffee.com/button-api/?text=커피한잔<br>후원하기&emoji=☕&slug=goohwan&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" />
         </a><br>
         <p style="text-align:center;">
-            <img src="https://tistory1.daumcdn.net/tistory/2714570/skin/images/mywalletqr.png" style="width:250px; display:block; margin:15px auto;" title="카메라 앱으로 QR코드를 비춰보세요">제작자 후원하기<br>(커피한잔 사주세요~*)
+            <img src="mywalletqr.png" style="width:250px; display:block; margin:15px auto;" title="카메라 앱으로 QR코드를 비춰보세요">제작자 후원하기<br>(커피한잔 사주세요~*)
         </p>
         """,
         unsafe_allow_html=True
